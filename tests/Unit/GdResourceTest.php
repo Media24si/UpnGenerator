0dc5899d7fa300d29dcbb2597c5cc4ae43437309
Bostjan <bostjan@muha.cc>
2025-12-26 16:57:02 +0100

    remove snapshot test for png generation (due to diff in gd versions)

--- tests/Unit/GdResourceTest.php
+++ tests/Unit/GdResourceTest.php
@@ -8,11 +8,6 @@ it('generates GD resource', function () {
     expect($gdResource)->toBeInstanceOf(\GdImage::class);
 });
 
-it('generates same png as snapshot', function () {
-    expect(md5(getDefaultUpn()->png()))
-        ->toMatchSnapshot();
-});
-
 it('generates GD resource for empty duedate', function () {
     $upn = getDefaultUpn();
     $upn->setDueDate(null);
